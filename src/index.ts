24cbc8b0badde2b306abe960e75ea544cc085a96
Mason Rhodes <mason.rhodes@outlook.com>
2022-12-19 20:15:22 -0800

    fix: stability

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { createNewClient } from "./client"
 
+if (!process.env.TOKEN) {
+  console.error("No token provided. Please set the TOKEN environment variable.")
+  process.exit(1)
+}
+if (!process.env.CLIENT_ID) {
+  console.error("No client ID provided. Please set the CLIENT_ID environment variable.")
+  process.exit(1)
+}
+
 createNewClient()
-
-
 // restart the bot on failure
 process.on('unhandledRejection', error => {
   console.error('Unhandled promise rejection:', error);
