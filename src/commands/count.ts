991c844ef3a4b05e963fcd52c17bcb79357d9959
Mason Rhodes <mason.rhodes@outlook.com>
2022-12-20 08:11:03 -0800

    fix: await initial reply

--- src/commands/count.ts
+++ src/commands/count.ts
@@ -1,4 +1,4 @@
-import {setTimeout} from 'node:timers/promises';
+import { setTimeout } from 'node:timers/promises';
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 
@@ -13,24 +13,28 @@ export default {
     ),
   async execute(interaction: ChatInputCommandInteraction) {
     const to = interaction.options.getNumber('to');
-    if (to === null) {
-      await interaction.reply('You must provide a number to count to');
-      return;
-    }
-    if (to < 1) {
-      await interaction.reply('I can only count to numbers greater than 0');
-      return;
-    }
-    if (to > 100) {
-      await interaction.reply('I can only count to 100');
-      return;
-    }
-    let msg = '1 ';
-    interaction.reply(msg);
-    for (let i = 2; i <= to; i++) {
-      msg += i + ' ';
-      await setTimeout(500);
-      await interaction.editReply(msg);
+    try {
+      if (to === null) {
+        await interaction.reply('You must provide a number to count to');
+        return;
+      }
+      if (to < 1) {
+        await interaction.reply('I can only count to numbers greater than 0');
+        return;
+      }
+      if (to > 100) {
+        await interaction.reply('I can only count to 100');
+        return;
+      }
+      let msg = '1 ';
+      await interaction.reply(msg);
+      for (let i = 2; i <= to; i++) {
+        msg += i + ' ';
+        await setTimeout(500);
+        await interaction.editReply(msg);
+      }
+    } catch (e) {
+      console.error(e);
     }
   },
 };
